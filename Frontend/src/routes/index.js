1a2e60c514af0b0b47d177bbd5c9e692c811fa46
Shane Ho <145674908+FS0-range@users.noreply.github.com>
2024-03-26 22:11:39 +0800

    feat - Added page contents for Login and IndivPoolPage

--- Frontend/src/routes/index.js
+++ Frontend/src/routes/index.js
@@ -1,15 +1,27 @@
 
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from "../pages/Home.vue";
+import PoolPage from "../pages/PoolPage.vue";
 import OrderCanceled from '../components/OrderCanceled.vue'
 import OrderSuccess from '../components/OrderSuccess.vue'
+import Login from "../pages/Login.vue"
+import IndividualPoolPage from "../pages/IndividualPoolPage.vue"
 
 
 const routes =[
     {
         path:"/",
-        name:"home",
-        component: Home,
+        name:"PoolPage",
+        component: PoolPage,
+    },
+    {
+      path: "/login",
+      name: "Login",
+      component: Login,
+    },
+    {
+      path: "/ipp",
+      name: "IndividualPoolPage",
+      component: IndividualPoolPage,
     },
     {
         path: '/success',
